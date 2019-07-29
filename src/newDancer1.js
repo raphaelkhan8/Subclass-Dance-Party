1cec3d281b49dfaa2c12fc8e3715f5d27f5db6c6
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-07-29 15:07:17 -0500

    added a new dancer

---
+++ src/newDancer1.js
@@ -0,0 +1,15 @@
+const NewDancer1 = function (top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('newDancer1');
+  this.$node.append('<img src = "https://i.ibb.co/fMPNgfm/rsz-oreo-belly.jpg" alt="rsz-oreo-belly" border="0">');
+};
+
+NewDancer1.prototype = Object.create(Dancer.prototype);
+NewDancer1.prototype.constructor = NewDancer1;
+
+NewDancer1.prototype.step = function () {
+  Dancer.prototype.step.call(this);
+  // this.$node.toggle();
+};
+
+window.NewDancer1 = NewDancer1;
