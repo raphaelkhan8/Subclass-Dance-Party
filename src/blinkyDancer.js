cec7e899b5bfaffd88c0586b68854b80578bfb66
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-07-30 17:22:31 -0500

    commented in working functionality

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,52 +1,23 @@
-const makeBlinkyDancer = function (top, left, timeBetweenSteps) {
-  const blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+const BlinkyDancer = function(top, left, timeBetweenSteps) {
+  // import Dancer to make BlinkyDancer an instance of the Dancer supe constructor
+  Dancer.call(this, top, left, timeBetweenSteps);
+  // this.$node.addClass('blinkyDancer');
+}
 
-BlinkyDancer.prototype = Object.create(Dancer.prototype);
-BlinkyDancer.prototype.constructor = BlinkyDancer;
+  // Import the methods of the Dancer superclass
+  BlinkyDancer.prototype = Object.create(Dancer.prototype);
 
-// const oldStep = blinkyDancer.step;
+  // Reset the constructor to of BlinkyDancer to BlinkyDancer
+  BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-  blinkyDancer.step = function () {
+  // Call the Dancer step method, which repeatedly calls the step method
+  BlinkyDancer.prototype.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
+    const BlinkyDancer = this;
+    const oldStep = Dancer.prototype.step;
+    oldStep.call(BlinkyDancer);
+    // Makes BlinkyDancer "blink" on and off
+    this.$node.toggle();
   };
 
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
-};
-
-window.makeBlinkyDancer = makeBlinkyDancer;
-
-
-
-
-
-// const BlinkyDancer = function(top, left, timeBetweenSteps) {
-//   // import Dancer to make BlinkyDancer an instance of the Dancer supe constructor
-//   Dancer.call(this, top, left, timeBetweenSteps);
-//   // this.$node.addClass('blinkyDancer');
-// }
-
-//   // Import the methods of the Dancer superclass
-//   BlinkyDancer.prototype = Object.create(Dancer.prototype);
-
-//   // Reset the constructor to of BlinkyDancer to BlinkyDancer
-//   BlinkyDancer.prototype.constructor = BlinkyDancer;
-
-//   // Call the Dancer step method, which repeatedly calls the step method
-//   BlinkyDancer.prototype.step = function() {
-//     // call the old version of step at the beginning of any call to this new version of step
-//     const BlinkyDancer = this;
-//     const oldStep = Dancer.prototype.step;
-//     oldStep.call(BlinkyDancer);
-//     // Makes BlinkyDancer "blink" on and off
-//     this.$node.toggle();
-//   };
-
-// window.BlinkyDancer = BlinkyDancer;
+window.BlinkyDancer = BlinkyDancer;
